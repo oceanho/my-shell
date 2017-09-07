9199bbaea2bd23fa2bc4c5a4265cd983751c114a
OceanHo <gzhehai@foxmail.com>
2017-09-07 16:47:56 +0800

    add rsync client scirpts & files

---
+++ scripts/rsync/client/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Install & Configure Rsync's Client side
+# Created by OceanHo(gzhehai@foxmail.com) at 2017-09-07
+#
+
+Auth_PasswordFile="/etc/rsync.password"
+Process_LogFile="/tmp/oceanh_yum-install-rsync.log"
+
+# Basic check
+if [ ! -f "./rsync.password" ] ; then
+   echo "missing file rsync.password in `pwd`"
+   exit 1
+fi
+
+# Check & install rsync if need do.
+rpm -q rsync >/dev/null 2>&1
+if [ ! $? -eq 0 ] ; then
+   echo "installing rsync."
+   yum install rsync -y >$Process_LogFile 2>&1
+   if [ ! $? -eq 1 ] ; then
+      echo "Install rsync failed. Messages: `cat $Process_LogFile`"
+      exit 1
+   else
+      echo "done."
+   fi
+then
+
+# 01. Create Rsync's Password file
+if [ ! -f $Auth_PasswordFile ] ; then
+   cp ./rsync.password /etc/ -f
+else
+   if [ $Process_ConfigureMode == "force" ] ; then
+      cp ./rsync.password /etc/ -f
+   fi
+fi
+if [ -f $Auth_PasswordFile ] ; then
+   chmod 600 $Auth_PasswordFile && chown root.root $Auth_PasswordFile
+   if [ ! $? -eq 0 ] ; then
+      echo "Rsync's Password process failed."
+      exit 0
+fi
---
+++ scripts/rsync/client/rsync.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Use Rsync push data to server in client side
+# Created by OceanHo(gzhehai@foxmail.com) at 2017-09-07
+#
+
+# Rsync Dir
+Rsync_Dir=$1
+
+# Rsync Server Info. Example rsyc@backup::backup/data
+Rsync_SvrInfo=$2
+
+# Rsync Server Password. It's Can be a file or text
+Rsync_Password=$3
+
+# Rsync to Client RATE limit. defualts 20MB/s.
+Rsync_BWlimit=20
+
+# Basic Check
+if [ -z $Rsync_Dir ] ; then
+   echo "invalid Rsync dir."
+   exit 1
+fi
+
+if [ $Rsync_Dir == "help" ] ; then
+   echo "Usage: $0 /data rsync@172.16.1.41::backup rsync_password 20"
+   exit 200
+fi
+
+if [ ! -f $Rsync_Dir -a ! -d $Rsync_Dir ] ; then
+   echo "invalid Path: $Rsync_Dir"
+   exit 1
+fi
+
+if [ -z $Rsync_SvrInfo ] ; then
+   echo "invalid Rsync Info."
+   exit 1
+fi
+
+if [ -z $Rsync_Password ] ; then
+   echo "invalid Rsync's Password."
+   exit 1
+fi
+
+# If configured the Number of 4 Parameter. Check and set bwlimit if passed.
+if [ ! -z "$4" ] ; then
+   if egrep "^[1-9][0-9]{0,}$" <<< "$4" >/dev/null 2>&1 ; then
+      Rsync_BWlimit=$4
+   fi
+fi
+
+# If the password is a file. Read that text as Password.
+if [ -f $Rsync_Password ] ; then
+   Rsync_Password=$(cat $Rsync_Password)
+fi
+
+# Configure Rsync's Password
+export RSYNC_PASSWORD=$Rsync_Passwu
+
+# Execute rsync process
+rsync -az --bwlimit $Rsync_BWlimit --delete $Rsync_Dir $Rsync_SvrInfo
