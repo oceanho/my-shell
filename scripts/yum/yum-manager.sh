492c5746197ed0147a31553f5da1ee1dbaa2cbf5
hehai <gzhehai@foxmail.com>
2017-10-01 21:01:56 +0800

    add yum-manager.sh

---
+++ scripts/yum/yum-manager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# A tool scripts for yum manager
+# Added By OceanHo(gzhehai@foxmail.com) at 2017-10-01
+#
+
+function chage_yum_to_aliyun()
+{
+    ver=`uname -r | sed -nr 's#.*el([0-9]).*#\1#gp'`
+    wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-${ver}.repo
+	wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-${ver}.repo
+}
+
