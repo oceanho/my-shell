cba386ecc144acf2ab9ffac537fbb3a26754c4b8
Ocean Ho <gzhehai@foxmail.com>
2017-10-05 18:27:23 +0800

    Update yum-manager.sh

--- scripts/yum/yum-manager.sh
+++ scripts/yum/yum-manager.sh
@@ -4,10 +4,10 @@
 # Added By OceanHo(gzhehai@foxmail.com) at 2017-10-01
 #
 
-function chage_yum_to_aliyun()
+function change_yum_to_aliyun()
 {
     ver=`uname -r | sed -nr 's#.*el([0-9]).*#\1#gp'`
     wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-${ver}.repo
-	wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-${ver}.repo
+    wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-${ver}.repo
 }
 
