14d20b976e45709e9e45e2fc19e91d93076f1aa1
OceanHo <gzhehai@foxmail.com>
2017-10-08 12:31:02 +0800

    clean

--- scripts/remount.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#
-# for mp in `cat /proc/mounts | awk '/172.16.1.31/{print $2}'`
-# do
-#    umount -lf $mp
-# done
-# 
--- scripts/rotate_nginx_access_log.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# rotate nginx's access log
-/usr/sbin/logrotate -f$1 /etc/logrotate.d/nginx_access
