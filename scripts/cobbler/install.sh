5371a58424925db2027531eb9bcf343d4d5659cb
Ocean Ho <gzhehai@foxmail.com>
2017-10-23 15:42:29 +0800

    fix remarks

--- scripts/cobbler/install.sh
+++ scripts/cobbler/install.sh
@@ -61,9 +61,10 @@ cat <<EOF
     sh $# cobbler-server-ip=auto cobbler-next-server-ip=auto net-auto-dev=ens34
     \n
     \n
-    2、手动指定cobbler-server-ip 与 next-server-ip，dhcp.template 的子网,掩码等信息自动从eth1网卡上获取\n
-    -------------------------------------------------------------------------------------------\n
-    sh $# cobbler-server-ip=auto cobbler-next-server-ip=auto net-auto-dev=ens34 \n
+    2、手动指定cobbler-server-ip 与 next-server-ip，dhcp.template 的子网,掩码等信息自动从eth1网卡上获取 \n
+    --------------------------------------------------------------------------------------------------------
+    \n
+    sh $# cobbler-server-ip=172.16.1.200 cobbler-next-server-ip=172.16.1.200 net-auto-dev=ens34 \n
     \033[0m
 EOF
     `
