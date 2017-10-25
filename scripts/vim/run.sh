4bc48155c313944d6298cf7d789b70f89d128b23
OceanHo <gzhehai@foxmail.com>
2017-10-26 01:41:42 +0800

    add vim files

---
+++ scripts/vim/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Todo:
+# Configure My Shell scripts Copyright Version info
+# Created By OceanHo(gzhehai@foxmail.com) AT 2017-10-26 01:39:19
+#
+
+/bin/cp -f ./files/vimrc ~/.vimrc
