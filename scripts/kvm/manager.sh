d0da21aeac9b5f56bca0610162d50c9a460dd341
OceanHo <gzhehai@foxmail.com>
2017-10-26 02:58:11 +0800

    fixs

--- scripts/kvm/manager.sh
+++ scripts/kvm/manager.sh
@@ -2,6 +2,7 @@
 #
 # KVM Manager tools
 # Created By OceanHo(gzhehai@foxmail.com) AT 2017-10-26
+#
 
 #
 # The kvm manager is only allowed root do.
@@ -30,6 +31,8 @@ if [ ! -f $EtcScriptRootDir/lib/oceanho-echo-lib.sh  ];then
 fi
 source $EtcScriptRootDir/lib/oceanho-echo-lib.sh
 
+#
+# Show help text
 function help()
 {
    echoInfo "`
@@ -42,6 +45,81 @@ function help()
    /bin/sh $0 create|update|clone|start|stop|delete|status
    EOF
    `"
+}
+
+
+#
+# Show KVM machine's status
+function list()
+{
+   virsh list --all
+}
+
+
+#
+# Clone a KVM machine
+# $1: 指定从哪个kvm虚机克隆
+# $2: 指定克隆的kvm新虚拟机的名称
+function clone()
+{
+   [ "$1" == "" ] && {
+      echoError "缺少克隆主机名,可以通过第1个参数指定,比如：c72-moban"
+      return 1
+   }
+   [ "$2" == "" ] && {
+      echoError "缺少克隆目标主机名,可以通过第2个参数指定,比如：c72-clone"
+      return 1
+   }
+
+   src_vir_disk="`virsh dumpxml $1 | sed -nr 's#.*<source file=.(.*).{3}#\1#gp'`"
+   dest_vir_disk="${2}.`basename $src_vir_disk`"
+   dest_vir_xmlconf="`dirname $src_vir_disk`/${2}.xml"
+
+   # 基础检查
+   if [ -f $dest_vir_disk ]
+   then
+      echoWarn "目标主机好像已经存在,请确认重试.磁盘:$dest_vir_disk"
+      return 1
+   fi
+   if [ -f $dest_vir_xmlconf ]
+   then
+      echoWarn "目标主机好像已经存在,请确认重试.配置:$dest_vir_xmlconf"
+      return 1
+   fi
+
+   # 复制虚拟磁盘
+   /bin/cp -a $src_vir_disk $dest_vir_disk
+   # 复制虚拟机配置文件
+   virsh dumpxml "$1" > $dest_vir_xmlconf
+
+   # 更新虚拟机的配置文件
+   new_kvm_conf "$dest_vir_xmlconf" "$2" "$dest_vir_disk"
+}
+
+
+#
+# 更新指定配置文件的UUID,Name,磁盘文件,网卡MAC地址等参数
+# $1：指定需要修改的xml的配置文件
+# $2：指定主机的新名称
+# $3：指定主机的新磁盘文件
+function new_kvm_conf()
+{
+   xml="$1"
+   new_name="$2"
+   new_disk="$3"
+   new_mac_addr=`openssl rand -hex 6 | sed -r 's/(..)/\1:/g;s/.$//g'`
+
+   # 1.替换虚拟机的名字
+   sed -ri "s#(.*)<name>.*</name>#\1<name>$new_name</name>#g" $xml
+   
+   # 2.替换虚拟机的UUID
+   sed -ri "s#(.*)<uuid>.*</uuid>#\1<uuid>`uuidgen`</uuid>#g" $xml
+   
+   # 3.替换虚拟机的磁盘文件
+   sed -ri "s#(.*<source file=).*#\1\'$new_disk\' />#g" $xml
+   
+   # 4.替换虚拟机的网卡的Mac地址
+   sed -ri "s#(.*<mac address=).*#\1\'${new_mac_addr}\' />#g" $xml
 }
 
 
