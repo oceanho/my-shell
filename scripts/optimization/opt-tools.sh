d8e2d3f6569bafc31abc78a2e2152f1fcaee0bde
OceanHo <gzhehai@foxmail.com>
2017-11-10 01:25:09 +0800

    fix bugs

--- scripts/optimization/opt-tools.sh
+++ scripts/optimization/opt-tools.sh
@@ -25,7 +25,7 @@ function opt_services()
    do
       chkconfig $service off
    done
-   for service in `chkconfig --list | awk -F "[ ]+" "/$services/{print \$1}"`
+   for service in `chkconfig --list | awk -F "[ ]+" '{print $1}' | awk "/$services/{print \$1}"`
    do
       chkconfig $service on --levels 35
    done
