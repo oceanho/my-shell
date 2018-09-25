2bab08841bfba2c03c9197201fff3015e4621b4a
Ocean Ho <gzhehai@foxmail.com>
2018-09-25 12:00:44 +0800

    Create gre.sh

---
+++ scripts/network/gre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# A gre tunnel manager tool script
+#
+
+ip_cmd="/sbin/ip"
+
+function checkAndAddGre(){
+  local remote=$1
+  local local=$2
+}
+
+function addGreRouter(){
+   ${ip_cmd} 
+}
